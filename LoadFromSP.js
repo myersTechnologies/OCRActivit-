121a5ece3e7ca45b4155bfef2a0ee5f6ae452808
Marco <marco.dasilva.wanted@gmail.com>
2019-02-19 15:37:00 +0100

    How to Load an int from SharedPreferences

--- LoadFromSP.js
+++ LoadFromSP.js
@@ -5,7 +5,9 @@
 
 	private SharedPreferences preferences;
 	private static final String PREF_KEY_NAME_1 = "PREF_KEY_NAME_1";
+	private static final int PREF_KEY_SCORE_1= "PREF_KEY_SCORE_1";		
 	private String NAME = "firstname";
+	private int SCORE = "score";
 
 	@Override
 		protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +17,13 @@
 		//Load default SharedPreferences, it's better to Load in MODE_PRIVATE
 		 preferences = PreferenceManager.getDefaultSharedPreferences(this);
 
-		 //Load the content you want to display, return null if there is no 
-	 data
+		 //Load the content you want to display, return null if there is no data
 
 		 NAME = preferences.getString(PREF_KEY_NAME_1, null);
+
+		//Load an int from SharedPreferences, return 0 if there is no data
+
+		SCORE = preferences.getInt(PREF_KEY_SCORE_1, 0);
 
 		}
 	}
